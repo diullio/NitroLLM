238fa37bd69b76a49fbf1c28fa3a3c57ec1cdac4
diullio <diullio@hotmail.com>
2025-01-07 16:45:21 -0300

    ajuste funcao

--- nitro.py
+++ nitro.py
@@ -31,45 +31,43 @@ def main():
         # A validação de risco agora é sempre feita
         risco = st.number_input("Risco Global Calculado", min_value=0, key="risco")
         
-        # Predição opcional
-        predicao_necessaria = st.checkbox("Necessita Predição de Ashworth?", key="predicao")
-
-    if predicao_necessaria:
-        with col3:
-            nitrosamina = st.text_input("Nitrosamina", key="nitrosamina")
-            limite = st.text_input("Limite de Ingestão Diário (ng/dia)", key="limite")
-            dose = st.text_input("Dose Máxima Diária (mg/dia)", key="dose")
-            # Validação para garantir que o valor seja numérico
-            limite = validar_float(limite)
-            dose = validar_float(dose)
-            ph = st.selectbox("Valor de pH", options=[3.15, 5, 7, 9], key="ph")
-            pka = st.slider("pKa", min_value=9.5, max_value=14.0, step=0.1, value=9.5, key="pka")
-            nitrito = st.selectbox("Níveis de Nitrito", options=["0.01 mg/L", "3 mg/L", "1 M"], key="nitrito")
-            amina = st.selectbox("Quantidade de Amina", options=["1 mM", "1 M"], key="amina")
-            temperatura = st.selectbox(
+    with col3:
+        st.subheader("Predição de Ashworth")
+        nitrosamina = st.text_input("Nitrosamina", key="nitrosamina")
+        limite = st.text_input("Limite de Ingestão Diário (ng/dia)", key="limite")
+        dose = st.text_input("Dose Máxima Diária (mg/dia)", key="dose")
+        # Validação para garantir que o valor seja numérico
+        limite = validar_float(limite)
+        dose = validar_float(dose)
+        ph = st.selectbox("Valor de pH", options=[3.15, 5, 7, 9], key="ph")
+        pka = st.slider("pKa", min_value=9.5, max_value=14.0, step=0.1, value=9.5, key="pka")
+        nitrito = st.selectbox("Níveis de Nitrito", options=["0.01 mg/L", "3 mg/L", "1 M"], key="nitrito")
+        amina = st.selectbox("Quantidade de Amina", options=["1 mM", "1 M"], key="amina")
+        temperatura = st.selectbox(
                 "Temperatura (°C)", options=["25", "35", "45", "55", "25 (1 h)"], key="temperatura"
-            )
+        )
 
-            if st.button("Calcular Predição"):
-                try:
-                    if limite is None or dose is None:
-                        st.error("Por favor, insira um valor válido para os parâmetros.")
-                    valor_tabela = localizar_ppb(amina, nitrito, temperatura, ph)
-                    if valor_tabela == "Combinação inválida":
-                        st.error("A combinação selecionada não existe no artigo.")
-                    quadro = criar_quadro(ph, pka, nitrito, amina, temperatura, dose, limite)
-                    texto = criar_texto(ph, pka, nitrito, amina, temperatura, valor_tabela, nitrosamina, ifa, limite, dose)
-                    html_anexo = gerar_html(ifa, quadro, texto)
-                    st.download_button(
-                        "Baixar Anexo Predição",
-                        data=html_anexo,
-                        file_name=f"Anexo_Predicao_{ifa}.html",
-                        mime="text/html",
-                    )
-                    # Salvar os valores de risco, nitrosamina e risco_nitrosamina no session_state
-                    risco_nitrosamina = "baixo" if (limite / dose > valor_tabela) else "alto"
-                except Exception as e:
-                    st.error(f"Erro no cálculo: {e}")
+        if st.button("Calcular Predição"):
+            try:
+                if limite is None or dose is None:
+                    st.error("Por favor, insira um valor válido para os parâmetros.")
+                valor_tabela = localizar_ppb(amina, nitrito, temperatura, ph)
+                if valor_tabela == "Combinação inválida":
+                    st.error("A combinação selecionada não existe no artigo.")
+                # Salvar os valores de risco, nitrosamina e risco_nitrosamina no session_state
+                risco_nitrosamina = "baixo" if (limite / dose > valor_tabela) else "alto"
+                quadro = criar_quadro(ph, pka, nitrito, amina, temperatura, dose, limite)
+                texto = criar_texto(ph, pka, nitrito, amina, temperatura, valor_tabela, nitrosamina, ifa, limite, dose)
+                html_anexo = gerar_html(ifa, quadro, texto)
+                st.download_button(
+                    "Baixar Anexo Predição",
+                    data=html_anexo,
+                    file_name=f"Anexo_Predicao_{ifa}.html",
+                    mime="text/html",
+                )
+                
+            except Exception as e:
+                st.error(f"Erro no cálculo: {e}")
 
     # Exibe a lista de IFAs adicionados com a opção de removê-los
     st.subheader("IFAs Adicionados")
@@ -98,43 +96,26 @@ def main():
 
     # Botão para adicionar um novo IFA
     if st.button("Adicionar IFA"):
-        # Se a predição for necessária, valide as variáveis antes de adicionar o IFA
-        if predicao_necessaria:
-            # Confirme que todas as variáveis de risco são válidas
-            if limite is None or dose is None or nitrosamina == "" or risco == "":
-                st.error("Por favor, preencha todos os campos de predicação corretamente antes de adicionar o IFA.")
-            else:
-                # Adiciona o IFA ao session_state com as informações recuperadas
-                st.session_state.dados.append(
-                    {
-                        "ifa": ifa,
-                        "fabricante": fabricante,
-                        "planta_fabril": planta_fabril,
-                        "difa": difa,
-                        "risco": risco,
-                        "nitrosamina": nitrosamina,
-                        "risco_nitrosamina": "baixo" if (limite / dose > valor_tabela) else "alto",  # Calcular risco aqui
-                    }
-                )
-                dados_df = pd.DataFrame(st.session_state.dados)
-                st.dataframe(dados_df)
-                st.success(f"IFA '{ifa}' adicionado com sucesso!")
+        if limite and dose:
+            valor_tabela = localizar_ppb(amina, nitrito, temperatura, ph)
+            risco_nitrosamina = "baixo" if (limite / dose > valor_tabela) else "alto"
         else:
-            # Caso a predição não seja necessária, apenas adicione o IFA com o risco calculado
-            st.session_state.dados.append(
-                {
+            nitrosamina = None
+            risco_nitrosamina = None
+        st.session_state.dados.append(
+            {
                     "ifa": ifa,
                     "fabricante": fabricante,
                     "planta_fabril": planta_fabril,
                     "difa": difa,
                     "risco": risco,
-                    "nitrosamina": "",  # No caso de não haver nitrosamina
-                    "risco_nitrosamina": "",  # No caso de não haver risco nitrosamina
-                }
-            )
-            dados_df = pd.DataFrame(st.session_state.dados)
-            st.dataframe(dados_df)
-            st.success(f"IFA '{ifa}' adicionado com sucesso!")
+                    "nitrosamina": nitrosamina if nitrosamina else None,  # No caso de não haver nitrosamina
+                    "risco_nitrosamina": risco_nitrosamina if risco_nitrosamina else None,  # No caso de não haver risco nitrosamina
+            }
+        )
+        dados_df = pd.DataFrame(st.session_state.dados)
+        st.dataframe(dados_df)
+        st.success(f"IFA '{ifa}' adicionado com sucesso!")
 
     if st.button("Gerar Avaliação de Risco"):
         if not produto or not st.session_state.dados:
